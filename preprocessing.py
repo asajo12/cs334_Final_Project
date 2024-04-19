236e3ee8b57fd5a154b3c1e20975c40ac862dec9
asajo12 <122995990+asajo12@users.noreply.github.com>
2024-04-19 12:22:04 -0400

    Add files via upload

--- preprocessing.py
+++ preprocessing.py
@@ -3,7 +3,26 @@
     #categorize numerical and categorical values 
     # one-hot-encoding or min-max when needed 
     
-import argparse
+#import argparse
 import pandas as pd
-import numpy as np
+#import numpy as np
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+def load_dataset(file):
+    return pd.read_csv(file)
+
+def main():
+    file_path = 'Airbnb_Data.csv'
+    data = load_dataset(file_path)
+    
+    #checking if the data loading step is complete
+    print("Dataset loaded successfully.")
+    print("Number of rows:", data.shape[0])
+    print("Number of columns:", data.shape[1])
+    print("\nColumns in the dataset:")
+    print(data.columns)
+    print("\nFirst few rows of the dataset:")
+    print(data.head())
+    
+if __name__ == "__main__":
+    main()
