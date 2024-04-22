0dfa82ab73589cd1f8b3e99c38bdc093a2a960f1
Allison Ng <99372508+ngxallison@users.noreply.github.com>
2024-04-22 05:23:41 -0400

    Add files via upload
    Added GridsearchCV

--- AirbnbKNN.py
+++ AirbnbKNN.py
@@ -1,7 +1,7 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split
 from imblearn.over_sampling import SMOTE
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from imblearn.pipeline import Pipeline as ImblearnPipeline
 from sklearn.metrics import classification_report, accuracy_score
@@ -27,18 +27,34 @@ y = data['review_score_category'].astype('category')
 # Splitting the dataset 73/27
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.27, random_state=42)
 
-# Handling class imbalance with SMOTE and scaling features
+# Define the parameter grid for GridSearchCV
+param_grid = {
+    'knn__n_neighbors': [1, 3, 5, 7, 10, 13, 15],  # Adjust values as needed
+    'knn__metric': ['euclidean', 'manhattan']
+}
+
+# Create the pipeline with SMOTE, scaler, and KNN classifier
 pipeline = ImblearnPipeline([
     ('smote', SMOTE(random_state=42)),
-    ('scaler', StandardScaler()),
-    ('knn', KNeighborsClassifier(n_neighbors=5, metric='euclidean'))
+    ('scaler', MinMaxScaler()),
+    ('knn', KNeighborsClassifier())
 ])
 
-# Training
-pipeline.fit(X_train, y_train)
+# Create GridSearchCV object
+grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring= 'accuracy')
 
-# Evaluation
-y_pred = pipeline.predict(X_test)
+# Fit GridSearchCV to the training data
+grid_search.fit(X_train, y_train)
+
+# Get the best hyperparameters
+best_params = grid_search.best_params_
+print("Best Hyperparameters:", best_params)
+
+# Get the best model from GridSearchCV
+best_model = grid_search.best_estimator_
+
+# Evaluate the best model
+y_pred = best_model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 classification_rep = classification_report(y_test, y_pred)
 
